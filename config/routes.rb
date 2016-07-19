b9b8cbc61ca4becd2bcf4b7593d5878d33e6bf1d
Justin Seidl <aceeightofspades@gmail.com>
2016-07-18 21:23:52 -0500

    Set up very basic marketing page and close off application

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
 
-  resources :checkings
-  root 'home#index'
-
   devise_for :users
   resources :users, :only => [:show] do
     resources :transactions, only: [:index, :show, :edit, :update]
     resources :accounts, only: [:index]
   end
+
+  # Root, User Logged In
+  authenticated :user do
+    root 'home#index', as: :authenticated_root
+  end
+  # Root, User Logged Out
+  root 'pages#show', page: 'home'
+
+  resources :checkings
+
+  # Pages for Marketing Site
+  get '/*page' => 'pages#show'
 
   post '/users/:id/add_account', to: 'plaidapi#add_account'
   patch '/users/:id/update_accounts', to: 'plaidapi#update_accounts'
