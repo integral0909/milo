b7fce8a88b965f3f365b6989e9eb8e1771701dea
Rob Schwartz <robertschwar@gmail.com>
2016-07-27 20:56:50 -0500

    added rails admin to project for admin availability

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719010520) do
+ActiveRecord::Schema.define(version: 20160728014751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160719010520) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "public_token_id"
+    t.integer  "checking_id"
   end
 
   create_table "checkings", force: :cascade do |t|
@@ -82,6 +83,7 @@ ActiveRecord::Schema.define(version: 20160719010520) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "invited",                default: false
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
