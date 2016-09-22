f06702fdbaf968bf5ca73fd63236014199283863
Rob Schwartz <robertschwar@gmail.com>
2016-09-21 22:00:32 -0500

    adding funding source to user

--- app/services/dwolla.rb
+++ app/services/dwolla.rb
@@ -1,20 +1,48 @@
 require 'dwolla_swagger'
 
 # All Dwolla functionality will be held here
-class Dwolla
-    def create_account(user)
+module Dwolla
+
+    # Create user on Dwolla
+    def self.create_user(user)
+      # We don't save name in 2 seperate fields so append -Milo to the name
       request_body = {
-        :firstName => 'Milo',
-        :lastName => user.name,
-        :email => user.email,
+        :firstName => user.name,
+        :lastName => '-Milo',
+        :email => user.email
       }
 
       # Using DwollaSwagger - https://github.com/Dwolla/dwolla-swagger-ruby
       dwolla_customer_url = DwollaSwagger::CustomersApi.create(:body => request_body)
+      puts "Adding dwolla url to user"
+      ap dwolla_customer_url
 
-      # TODO: add dwolla customer URL to the user
+      # Add dwolla customer URL to the user
       user = User.find(user.id)
-      user.dwolla_url = dwolla_customer_url
+      user.dwolla_id = dwolla_customer_url
+      user.save!
+    end
+
+    # Add funding source for user to Dwolla
+    def self.connect_funding_source(user)
+      # Find the checking account associated with the user
+      user_checking = Checking.where(user_id: user.id)
+      # Get the info from the Account to add a funding source to Dwolla
+      funding_account = Account.where(plaid_acct_id: user_checking.plaid_acct_id)
+
+      customer_url = user.dwolla_id
+      request_body = {
+        routingNumber: funding_account.number,
+        accountNumber: funding_account.account_number,
+        type: funding_account.acct_subtype,
+        name: funding_account.name
+      }
+
+      funding_source = DwollaSwagger::FundingsourcesApi.create_customer_funding_source(customer_url, :body => request_body)
+
+      # Add the funding source to the user
+      user = User.find(user.id)
+      user.dwolla_funding_source = funding_source
       user.save!
 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816183203) do
+ActiveRecord::Schema.define(version: 20160922025823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20160816183203) do
     t.string   "referral_code"
     t.string   "name"
     t.string   "zip"
+    t.string   "dwolla_id"
+    t.string   "dwolla_funding_source"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
