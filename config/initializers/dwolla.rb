c2602c2f4148ca644acaafd4130043eca9d0fd3b
Rob Schwartz <robertschwar@gmail.com>
2016-10-05 09:05:53 -0500

    Round up task is good shape. Need to test the withdraw from user and deposit into our account

--- config/initializers/dwolla.rb
+++ config/initializers/dwolla.rb
@@ -9,18 +9,21 @@ $dwolla = DwollaV2::Client.new(id: ENV["DWOLLA_CLIENT_ID"], secret: ENV["DWOLLA_
   end
 end
 
-# create an application token if one doesn't already exist
-begin
-  TokenData.fresh_token_by! account_id: nil
-rescue ActiveRecord::RecordNotFound => e
-  $dwolla.auths.client # this gets saved in our on_grant callback
-end
+# Make sure there is at least 1 token otherwise the application will break
+if !TokenData.all.nil?
+  # create an application token if one doesn't already exist
+  begin
+    TokenData.fresh_token_by! account_id: nil
+  rescue ActiveRecord::RecordNotFound => e
+    $dwolla.auths.client # this gets saved in our on_grant callback
+  end
 
-# create an account token if one doesn't already exist
-begin
-  TokenData.fresh_token_by! account_id: ENV["DWOLLA_ACCOUNT_ID"]
-rescue ActiveRecord::RecordNotFound => e
-  TokenData.create! account_id: ENV["DWOLLA_ACCOUNT_ID"],
-                    refresh_token: ENV["DWOLLA_ACCOUNT_REFRESH_TOKEN"],
-                    expires_in: -1
+  # create an account token if one doesn't already exist
+  begin
+    TokenData.fresh_token_by! account_id: ENV["DWOLLA_ACCOUNT_ID"]
+  rescue ActiveRecord::RecordNotFound => e
+    TokenData.create! account_id: ENV["DWOLLA_ACCOUNT_ID"],
+                      refresh_token: ENV["DWOLLA_ACCOUNT_REFRESH_TOKEN"],
+                      expires_in: -1
+  end
 end
