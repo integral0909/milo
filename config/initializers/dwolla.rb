93582589a5a5935113e0bd0b3d479cb784d0907d
Rob Schwartz <robertschwar@gmail.com>
2016-10-20 07:14:07 -0500

    uncommenting TokenData

--- config/initializers/dwolla.rb
+++ config/initializers/dwolla.rb
@@ -10,17 +10,17 @@ $dwolla = DwollaV2::Client.new(id: ENV["DWOLLA_CLIENT_ID"], secret: ENV["DWOLLA_
 end
 
 # create an application token if one doesn't already exist
-# begin
-#   TokenData.fresh_token_by! account_id: nil
-# rescue ActiveRecord::RecordNotFound => e
-#   $dwolla.auths.client # this gets saved in our on_grant callback
-# end
-#
-# # create an account token if one doesn't already exist
-# begin
-#   TokenData.fresh_token_by! account_id: ENV["DWOLLA_ACCOUNT_ID"]
-# rescue ActiveRecord::RecordNotFound => e
-#   TokenData.create! account_id: ENV["DWOLLA_ACCOUNT_ID"],
-#                     refresh_token: ENV["DWOLLA_ACCOUNT_REFRESH_TOKEN"],
-#                     expires_in: -1
-# end
+begin
+  TokenData.fresh_token_by! account_id: nil
+rescue ActiveRecord::RecordNotFound => e
+  $dwolla.auths.client # this gets saved in our on_grant callback
+end
+
+# create an account token if one doesn't already exist
+begin
+  TokenData.fresh_token_by! account_id: ENV["DWOLLA_ACCOUNT_ID"]
+rescue ActiveRecord::RecordNotFound => e
+  TokenData.create! account_id: ENV["DWOLLA_ACCOUNT_ID"],
+                    refresh_token: ENV["DWOLLA_ACCOUNT_REFRESH_TOKEN"],
+                    expires_in: -1
+end
