1e27e59a3f42d092759cfc5a0e01f29cfd53d8db
Rob Schwartz <robertschwar@gmail.com>
2016-11-06 08:58:43 -0600

    check if plaid should be in production

--- config/initializers/argyle.rb
+++ config/initializers/argyle.rb
@@ -2,6 +2,9 @@ Argyle.configure do |config|
   config.key = ENV['PLAID_PUBLIC_KEY']          # Public key from Plaid
   config.secret = ENV['PLAID_SECRET']           # Secret from Plaid
   config.customer_id = ENV['PLAID_CLIENT_ID']   # Client ID from Plaid
-  # change to production when going live
-  config.env = :tartan
+  if Rails.env.production?
+    config.env = "tartan"
+  else
+    config.env = "production"
+  end
 end
