1c821b052dfe06bb1df403c0344c90dc7d579b04
Rob Schwartz <robertschwar@gmail.com>
2016-11-10 07:24:53 -0600

    pull in current weeks transactions for the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,18 +31,11 @@ class ApplicationController < ActionController::Base
       @accounts = Account.where(user_id: @user.id)
       # Find all checking accounts associated withe the user
       @checking = Checking.find_by(user_id: @user.id)
-      set_transactions
+      set_dwolla_id
     end
   end
 
-  def set_transactions
-    # If, checking account exists get the transactions for that account
-    @transactions = Transaction.where(account_id: @checking.plaid_acct_id) if @checking
-
-    set_dwolla_id
-  end
-
-  # Add Dwolla funding source to user if not already set
+  # Add Dwolla id to user if not already set NOTE: Depricate once all waitlist users sign up
   def set_dwolla_id
     if !@user.dwolla_id
       Dwolla.create_user(@user)
--- app/services/plaid_helper.rb
+++ app/services/plaid_helper.rb
@@ -26,4 +26,57 @@ module PlaidHelper
     end
   end
 
+  # ----------------------------------------------
+  # CURRENT-WEEK-TRANSACTIONS -------------------
+  # Pull in current weeks transactions from the connected user account and send to the view
+  # user : current_user that is logged in
+  # checking : Checking account connected to the current_user
+  # return : array of transaction objects for the view
+  # ----------------------------------------------
+  def self.current_week_transactions(user, checking)
+    if user && checking
+      # set date to beginning of the week
+      sunday = set_sunday
+
+      # Pull in plaid connect user
+      connect_user = Argyle.plaid_client.set_user(user.plaid_access_token, ['connect'])
+      user_transactions = connect_user.transactions()
+
+      # filter transactions to the ones that match the users checking and are only from the current week
+      current_transactions = user_transactions.select{|a| (a.account == checking.plaid_acct_id) && (a.date.to_date >= sunday)}
+
+      # create transaction objects for the view
+      transactions_for_view = current_transactions.map { |tr| {roundup: round_transaction(tr.amount), trans_name: tr.name, amount: tr.amount} }
+
+      return transactions_for_view
+    end
+  end
+
+  # ----------------------------------------------
+  # ROUND-TRANSACTIONS -------------------
+  # Set the amount that we would round up for the transaction shown
+  # amount : transaction amount
+  # return : amount we are going to round up
+  # ----------------------------------------------
+  def self.round_transaction(amount)
+    new_amount = amount.ceil
+
+    if new_amount > 0.00
+      subtract = new_amount - amount
+
+      subtract == 0 ? 1.00 : subtract.round(2)
+    else
+      0.00
+    end
+  end
+
+  # ----------------------------------------------
+  # SET-SUNDAY -------------------
+  # Set the previous Sunday's date
+  # ----------------------------------------------
+  def self.set_sunday
+    current_date = Date.today
+    sunday = current_date.beginning_of_week(start_day = :sunday)
+  end
+
 end
