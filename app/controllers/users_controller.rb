163bf6379a93eadece42cc52ef80e743427e3e08
Rob Schwartz <robertschwar@gmail.com>
2016-11-27 11:45:12 -0600

    Allow users to updated password from the settings/security page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@
 # RUBY->CONTROLLER->USERS-CONTROLLER =============
 # ================================================
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
 
   # ==============================================
   # ACTIONS ======================================
@@ -13,6 +15,29 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @goals = @user.goals
+  end
+
+  # ----------------------------------------------
+  # UPDATE-PASSWORD ------------------------------
+  # Allowing the user to update the password from the dashboard
+  # ----------------------------------------------
+  def update_password
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      # Sign in the user by passing validation in case their password changed
+      bypass_sign_in(@user)
+      flash[:notice] = "Password Updated"
+      redirect_to root_path
+    else
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      redirect_to settings_security_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 
 end
