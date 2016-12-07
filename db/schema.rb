039e4e75a6d903ddf35234289dbec0e553983765
Justin Seidl <aceeightofspades@gmail.com>
2016-12-07 11:15:58 -0600

    Removed address from business, will keep on user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201023707) do
+ActiveRecord::Schema.define(version: 20161207165608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,6 @@ ActiveRecord::Schema.define(version: 20161201023707) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name"
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
     t.decimal  "contribution", precision: 2
     t.datetime "created_at"
     t.datetime "updated_at"
