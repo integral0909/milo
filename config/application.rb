62001cca340f9682ccd22ab99362ec160cd227ce
Rob Schwartz <robertschwar@gmail.com>
2016-12-06 22:15:58 -0600

    test to fix admin

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module Milo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.enabled = true
+    config.middleware.use Rack::MethodOverride
+    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+
+
   end
 end
