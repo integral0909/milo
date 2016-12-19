82978bca9bc759f207063161b85de55e955857c7
Justin Seidl <aceeightofspades@gmail.com>
2016-12-18 18:54:42 -0600

    Add frequency to business migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212205459) do
+ActiveRecord::Schema.define(version: 20161219005347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20161212205459) do
     t.decimal  "contribution", precision: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "frequency"
   end
 
   create_table "checkings", force: :cascade do |t|
