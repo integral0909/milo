3ef0ae97c1c5f674ca430e61e72feda09f372eee
Rob Schwartz <robertschwar@gmail.com>
2017-04-09 17:46:43 -0400

    moved withdraw and quick save to background jobs

--- app/services/dwolla.rb
+++ app/services/dwolla.rb
@@ -326,49 +326,7 @@ module Dwolla
    # ----------------------------------------------
    # send funds the user requested to withdraw
    def self.send_funds_to_user(user, requested_amount)
-     begin
-       current_date = Date.today
-
-       transfer_request = {
-         :_links => {
-           :source => {
-             :href => "https://api-uat.dwolla.com/funding-sources/#{ENV["DWOLLA_FUNDING_SOURCE_FBO"]}"
-           },
-           :destination => {
-             :href => user.dwolla_id
-           }
-         },
-         :amount => {
-           :currency => "USD",
-           :value => requested_amount
-         },
-         :metadata => {
-           :customerId => user.id
-         }
-       }
-       Dwolla.set_dwolla_token
-       # Using DwollaV2 - https://github.com/Dwolla/dwolla-v2-ruby (Recommended)
-       transfer = @dwolla_app_token.post "transfers", transfer_request
-
-       current_transfer_url = transfer.headers[:location]
-
-       # Get the status of the current transfer
-       Dwolla.set_dwolla_token
-       transfer_status = @dwolla_app_token.get current_transfer_url
-       current_transfer_status = transfer_status.status
-
-       # Save the withdraw as a transfer. Params are the user, transfer_url, transfer_status, roundup_amount, roundup_count, transfer_type, current_date, tech_fee_charged
-       Transfer.create_transfers(user,"", current_transfer_url, current_transfer_status, requested_amount, "", "withdraw", current_date, false)
-
-       # decrease the requested amount from the user's account balance
-       User.decrease_account_balance(user, requested_amount)
-       # send email to user about funds being transfered to their account.
-       funding_account  = Checking.find_by_user_id(user.id)
-       BankingMailer.withdraw_start(user, requested_amount, funding_account).deliver_now
-     rescue => e
-       # send email to dev team about failed transfer to user
-       SupportMailer.user_withdraw_failed(user, requested_amount, e).deliver_now
-     end
+     DwollaSendFundsToUserJob.perform(user.id, requested_amount)
    end
 
   #  def self.transfer_tech_fee_to_corp(fee_amount)
@@ -403,51 +361,7 @@ module Dwolla
   #  end
 
   def self.quick_save(user, amount)
-    begin
-      current_date = Date.today
-
-      transfer_request = {
-        :_links => {
-          :source => {
-            :href => user.dwolla_funding_source
-          },
-          :destination => {
-            :href => "https://api-uat.dwolla.com/funding-sources/#{ENV["DWOLLA_FUNDING_SOURCE_FBO"]}"
-          }
-        },
-        :amount => {
-          :currency => "USD",
-          :value => amount
-        },
-        :metadata => {
-          :customerId => user.id
-        }
-      }
-
-      Dwolla.set_dwolla_token
-      # Using DwollaV2 - https://github.com/Dwolla/dwolla-v2-ruby (Recommended)
-      transfer = @dwolla_app_token.post "transfers", transfer_request
-
-      current_transfer_url = transfer.headers[:location]
-
-      # Get the status of the current transfer
-      Dwolla.set_dwolla_token
-      transfer_status = @dwolla_app_token.get current_transfer_url
-      current_transfer_status = transfer_status.status
-
-      # Save the withdraw as a transfer. Params are the user, transfer_url, transfer_status, roundup_amount, roundup_count, transfer_type, current_date, tech_fee_charged
-      Transfer.create_transfers(user,"", current_transfer_url, current_transfer_status, amount, "", "deposit", current_date, false)
-
-      # add the quick save amount from the user's account balance
-      User.add_account_balance(user, amount, true)
-      # send email to user about funds being transfered to their account.
-
-      BankingMailer.quick_save_success(user, amount).deliver_now
-    rescue => e
-      p e
-      # send email to dev team about failed transfer to user
-      SupportMailer.quick_save_failed(user, amount, e).deliver_now
-    end
+    DwollaQuickSaveJob.perform(user.id, amount)
   end
 
   # reset the dwolla app token
