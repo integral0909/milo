96e8f0729b126072e9a8313373d41ce9c000fa10
Justin Seidl <aceeightofspades@gmail.com>
2017-04-20 08:20:51 -0500

    Set auth token for each user on migration

--- db/migrate/20170324165240_add_authentication_token_to_users.rb
+++ db/migrate/20170324165240_add_authentication_token_to_users.rb
@@ -1,6 +1,9 @@
 class AddAuthenticationTokenToUsers < ActiveRecord::Migration
   def change
     add_column :users, :auth_token, :string, default: ""
+    User.find_each do |user|
+      user.generate_authentication_token!
+    end
     add_index :users, :auth_token, unique: true
   end
 end
