777066bcf7ebd1f3ef35610b5d7064c9f7fb8557
Justin Seidl <aceeightofspades@gmail.com>
2017-04-20 08:25:53 -0500

    Save

--- db/migrate/20170324165240_add_authentication_token_to_users.rb
+++ db/migrate/20170324165240_add_authentication_token_to_users.rb
@@ -3,6 +3,7 @@ class AddAuthenticationTokenToUsers < ActiveRecord::Migration
     add_column :users, :auth_token, :string, default: ""
     User.find_each do |user|
       user.generate_authentication_token!
+      user.save
     end
     add_index :users, :auth_token, unique: true
   end
