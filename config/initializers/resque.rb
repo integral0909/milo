0964f4bc460c35903ca6dab6dcff58afe5a4d56a
Rob Schwartz <robertschwar@gmail.com>
2017-04-19 21:07:28 -0500

    remove double init of redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,2 @@
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
-
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
