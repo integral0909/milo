2c244808c57d7164811f426e2dd6e364c188727a
Rob Schwartz <robertschwar@gmail.com>
2017-04-24 06:41:58 -0500

    added uri parse

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,6 @@
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
 if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 end
