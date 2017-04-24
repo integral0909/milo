d860f03bee7e1090b2f8a6bc27f334029248dd10
Rob Schwartz <robertschwar@gmail.com>
2017-04-24 06:40:25 -0500

    establish redis for rescue only on production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,5 @@
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
+if Rails.env.production?
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+end
